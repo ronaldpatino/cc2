8c4e11c3feb81c0519e648abe2e18be82da1c116
Ronald Patino <rpatino@itlligen.com>
2022-02-02 13:10:22 -0500

    refactor: Updated added data items as per doc

--- data.js
+++ data.js
@@ -1,19 +1,37 @@
 const products = [
     {
         "id": "3ce6c291-f353-422f-b822-33a031faa2cf",
-        "code": "CC1",
+        "code": "ACC1",
         "product": "Apple granny smith",
         "package": "40 LB",
-        "units": "3.638.066",
+        "units": "3,638,066",
         "category": "Fruits",
         "lastUpdated": formatDate(new Date())
     },
     {
         "id": "1ce6c291-f353-422f-b822-33a031faa2cf",
-        "code": "CC2",
+        "code": "ACC2",
         "product": "Pinaple crownless",
         "package": "7 CT",
-        "units": "6463",
+        "units": "6,646,020",
+        "category": "Fruits",
+        "lastUpdated": formatDate(new Date())
+    },
+    {
+        "id": "1ce7c291-f353-422f-b822-33a031faa2cf",
+        "code": "ACC3",
+        "product": "Banana green",
+        "package": "8 CT",
+        "units": "8,664,948",
+        "category": "Fruits",
+        "lastUpdated": formatDate(new Date())
+    },
+    {
+        "id": "1ce7c291-f353-422f-b822-33a031faa2cf",
+        "code": "ACC3",
+        "product": "Banana green tip",
+        "package": "40 LB",
+        "units": "2,592,335",
         "category": "Fruits",
         "lastUpdated": formatDate(new Date())
     }
